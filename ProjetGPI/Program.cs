ca2ffac75a41f859b3084c888dc325a32978ae7b
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-22 18:48:36 -0400

    Change program

--- ProjetGPI/Program.cs
+++ ProjetGPI/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetGPI.Data;
+using ProjetGPI.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//builder.Services.AddDbContext<ProjetGPIDbContext>(options => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ProjetGPIDB;Trusted_Connection=True;"));
+
+// Configure in memory database
+builder.Services.AddDbContext<ProjetGPIDbContext>(opt => opt.UseInMemoryDatabase("ProjetGPIDB"));
+builder.Services.AddScoped<ProjetGPIDbContext>();
+
+
 var app = builder.Build();
+
+// 2. Find the service within the scope to use
+using (var scope = app.Services.CreateScope())
+{
+    // 3. Get the instance of ProjetGPIDbContext in our service layer
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ProjetGPIDbContext>();
+
+    // 4. Call the SeedDataGenerator to generate seed data
+    SeedDataGenerator.Initialize(services);
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,8 +44,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Etudiants}/{action=Index}/{id?}");
 
 app.Run();
