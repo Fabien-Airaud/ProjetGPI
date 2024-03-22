4b9e31320413c2135865324db29526faf7efd539
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-22 17:34:36 -0400

    Add project files.

---
+++ ProjetGPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetGPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
