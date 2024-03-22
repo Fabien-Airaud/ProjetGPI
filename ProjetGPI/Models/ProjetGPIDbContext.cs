06358c24f5c8ed99c54e97cbaedcebe3163db425
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-22 17:49:52 -0400

    Create project Db Context

---
+++ ProjetGPI/Models/ProjetGPIDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetGPI.Models
+{
+    public class ProjetGPIDbContext : DbContext
+    {
+        public ProjetGPIDbContext(DbContextOptions<ProjetGPIDbContext> options)
+            : base(options) { }
+
+        public DbSet<Etudiant> Etudiants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Etudiant>(entity =>
+            {
+                entity.Property(e => e.Nom).IsFixedLength();
+                entity.Property(e => e.Prenom).IsFixedLength();
+                entity.Property(e => e.Email).IsFixedLength();
+                entity.Property(e => e.Sexe).IsFixedLength();
+            });
+        }
+    }
+}
