f94194f2285bab1531b079edf2e5ba66d9432b9e
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-25 14:40:02 -0400

    Add start test for Etudiant index

--- ProjetGPITests/EtudiantSeleniumTest.cs
+++ ProjetGPITests/EtudiantSeleniumTest.cs
@@ -1,7 +1,27 @@
-﻿namespace ProjetGPITests
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProjetGPITests
 {
     public class EtudiantSeleniumTest
     {
+        [Fact]
+        public void IndexEtudiantTest()
+        {
+            // Arrange
+            var chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl("https://localhost:7212/");
 
+            // Act
+            var navbarBrand = chromeDriver.FindElement(By.CssSelector("header nav p.navbar-brand"));
+            var etudiants = chromeDriver.FindElements(By.ClassName("etudiant"));
+
+            // Assert
+            Assert.Equal("ProjetGPIApp", navbarBrand.Text);
+            Assert.NotEmpty(etudiants);
+
+            // Cleanup
+            chromeDriver.Quit();
+        }
     }
 }
