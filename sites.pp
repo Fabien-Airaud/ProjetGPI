baaa91bc3b30e1f1e81781cb1cadef7e0d89d9cc
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-23 01:06:25 -0400

    Change creating pool

--- sites.pp
+++ sites.pp
@@ -30,6 +30,22 @@ $configurations_iis.each |$site, $config| {
         ensure  => directory,
     }
     # Création du pool d'application
+    if $config['application_pool'] in iis_application_pool {
+        exec { "restart-${config['application_pool']}-pool": # Redémarrage du pool d'application si déjà existant
+            command     => "C:\\Windows\\System32\\inetsrv\\appcmd.exe recycle apppool /apppool.name:${config['application_pool']}",
+            refreshonly => true,
+            require     => File[$config['physical_path']],
+        }
+    } else {
+        iis_application_pool {$config['application_pool']: # Création du pool d'application si inexistant
+            ensure                  => 'present',
+            state                   => 'started',
+            managed_runtime_version => 'v4.0',
+            managed_pipeline_mode   => 'Integrated',
+            require                 => File[$config['physical_path']], # On attend que le dossier physique soit créé
+        }
+    }
+    # # Cas utilisant un iis_application_pool pour la création du pool d'application
     # iis_application_pool {$config['application_pool']:
     #     ensure                  => 'present',
     #     state                   => 'started',
@@ -37,12 +53,13 @@ $configurations_iis.each |$site, $config| {
     #     managed_pipeline_mode   => 'Integrated',
     #       require                 => File[$config['physical_path']], # On attend que le dossier physique soit créé
     # }
-    exec { "create-${config['application_pool']}-pool":
-        command => "C:\\Windows\\System32\\inetsrv\\appcmd.exe add apppool /name:${config['application_pool']} /managedRuntimeVersion:v4.0 /managedPipelineMode:Integrated",
-        onlyif  => "C:\\Windows\\System32\\inetsrv\\appcmd.exe list apppool | findstr /i ${config['application_pool']}",
-        unless  => "C:\\Windows\\System32\\inetsrv\\appcmd.exe list apppool | findstr /i ${config['application_pool']}",
-        require => File[$config['physical_path']], # On attend que le dossier physique soit créé
-    }
+    # # Cas utilisant un exec pour la création du pool d'application
+    # exec { "create-${config['application_pool']}-pool":
+    #     command => "C:\\Windows\\System32\\inetsrv\\appcmd.exe add apppool /name:${config['application_pool']} /managedRuntimeVersion:v4.0 /managedPipelineMode:Integrated",
+    #     onlyif  => "C:\\Windows\\System32\\inetsrv\\appcmd.exe list apppool | findstr /i ${config['application_pool']}",
+    #     unless  => "C:\\Windows\\System32\\inetsrv\\appcmd.exe list apppool | findstr /i ${config['application_pool']}",
+    #     require => File[$config['physical_path']], # On attend que le dossier physique soit créé
+    # }
     # Création du site
     iis_site { $site:
         ensure          => 'started',
